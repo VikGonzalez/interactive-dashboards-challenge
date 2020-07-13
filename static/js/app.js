7e6f2852e5f502c3e1cf05f65a9cabbedbbe160f
VikGonzalez <victormgm@outlook.com>
2020-07-13 13:09:56 -0500

    updating files

--- static/js/app.js
+++ static/js/app.js
@@ -1,141 +1,106 @@
 
-// IMPORTAR JSON
-d3.json("./data/samples.json").then(f=> 
-    console.log(f.samples)
-    );
-
-//  CREAR LISTA
-let options = d3.json("./data/samples.json").then((d=>{
-    let subjectIds = [];
-
-    let select = document.getElementById("#selDataset")
-
-    let s = Object.values(d.samples);
-    let dropdownList = s.forEach(el => {return el.id})
-
-    console.log(options)
-
-    for (let i = 0; i < options.lenght; i++) {
-        let opt = options[i];
-        let el = document.createElement("option")
-
-        el.value = opt;
-
-        select.appendChild(el)
-    };
-
-function buildBarPlot() {
-    d3.json("./data/samples.json").then((d)=> {
-        let preset = "940";
-
-        let samples = d.samples;
-        let filteredResult = samples.filter(sampleObject => sampleObject.id == preset);
-        let result = filteredResult[0];
-        // RANKEAR TOP TEN
-        let sampleValues = result.sample_values.slice(0,10);
-        let otuIds = result.otu_ids.slice(0,10);
-        let otuLabels = result.otu_labels.slice(0,10);
-
-        // TRACE FOR OTUS DATA
-        let trace = {
-            y:otuIds,
-            name: name,
-            x:sampleValues,
-            width: 100,
-            // name: "name",
-            // text: otuLabels,
-            type: "bar",
-            orientation: "h"
-        };
-        // ASIGNAR DATA
-        let plotData = [trace];
-        // APLICAR EL GROUP BAR AL LAYOUT
-        let plotLayOut = {
-            title: "Top 10 OTUS",
-            width: 1000,
-            height: 1000,
-
-            yaxis: {title: "OTU ID"}
-        };
-        // DEFINIR EL PLOT A UTILIZAR CON EL DIV TAG ID "BAR"
-        Plotly.newPlot("bar", plotData, plotLayOut);
-        console.log(result);
-        console.log(sampleValues);
-        console.log(otuIds);
-        console.log(otuLabels);
-    });
-};
-
-buildBarPlot();
-
-function buildBubblePlot() {
-    d3.json("./data/samples.json").then((d)=> {
-        let preset = "1510";
-
-        let samples = d.samples;
-        let filteredResult = samples.filter(sampleObject => sampleObject.id == preset);
-        let result = filteredResult[0];
-        // RANKEAR TOP TEN
-        let sampleValues = result.sample_values.slice(0,10);
-        let otuIds = result.otu_ids.slice(0,10);
-        let otuLabels = result.otu_labels.slice(0,10);
-
-        // TRACE FOR OTUS DATA
-        let trace = {
-            y:otuIds,
-            x:sampleValues,
-            text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-            mode: 'markers',
-            marker: {
-              color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-              size: [40, 60, 80, 100]
-            }
-          };
-          
-          let plotData = [trace];
-          
-          let plotLayOut = {
-            title: 'Bubble Chart Hover Text',
-            showlegend: false,
-            height: 600,
-            width: 600
-          };
-          
-          Plotly.buildBubblePlot('bubble', plotData, plotLayOut);
-
-    });
-};
-
-buildBubblePlott();
-
-
-// CREAR FUNCION PARA TRAER DATA
-function getData() {
-    let selectedOption = d3.select("#selDataset").node().value;
-    // CREAR ESCUCHADOR Y ALMACENARLA !!!!!REVISAR EL ID
-    let choice = d3.selectAll("#selDataset").on("change", getData);
-    // ASIGNAR A UNA VARIABLE LA INFORMACIÓN QUE INGRESO EL USUARIO
-    let subjectIds = selectedOption.property("value");
-
-
-    let options = d3.json("./data/samples.json").then((d)=>{
-        let subjectIds = [];
-
-        let select = document.getElementById("#selDataset");
-
-        let s = Object.values(d.samples);
-        let dropdownList = s.forEach(el => {return el.id})
-
-        console.log(options)
-
-        for( i = 0; i < options.lenght; i++) {
-            let opt = options[i];
-            let el = document.createElement("option");
-
-            el.value = opt;
-
-            select.appendChild(el)
+d3.json("./data/samples.json").then((importedData) => {
+    console.log("importedEntireData", importedData);
+    let navelData = importedData
+    let sampleData = navelData.samples
+    let meta = navelData.metadata
+  
+    let masterId = sampleData[0].id
+  
+    function makePlotsTable () {
+  
+      let dataPlot = []
+  
+      
+      sampleData.forEach(navelSample => {
+        if (masterId === navelSample.id) {
+          dataPlot.push(navelSample.sample_values);
+          dataPlot.push(navelSample.otu_ids);
+          dataPlot.push(navelSample.otu_labels);
+      
+        }  
+      });
+      console.log("gotSampleDataSucceeded", dataPlot)
+  
+      let metaTable = []
+  
+  
+      meta.forEach(subjectMeta => {
+        if (masterId === subjectMeta.id) {
+          metaTable.push(subjectMeta.ethnicity)
         }
-    });
-    }   
-}));
+      });
+  
+      console.log("metaTable", metaTable)
+      console.log("masterID", masterId)
+      console.log("meta", meta)
+  
+      // DEFINIR NOMBRE DE ETIQUETAS PARA SAMPLE ID
+      let yFor = dataPlot[1].map(function(el){
+        return "OTU" + String(el)
+      });
+  
+      // CREAR TRACE PARA BAR PLOT
+      let barTrace = [{
+        x:dataPlot[0].slice(0,10).reverse(),
+        y: yFor.slice(0,10).reverse(),
+        text: dataPlot[2].slice(0,10),
+        type: "bar",
+  
+        orientation: "h"
+      }];
+      // CREAR LAYOUT PARA BAR PLOT
+      let barLayout = {
+        title: "Top 10 OTUs Found",
+        colorway: ['#f3cec9', '#e7a4b6', '#cd7eaf', '#a262a9', '#6f4d96', '#3d3b72', '#182844'],
+        xaxis: {title: "OTU Value"}
+      };
+      // CREAR BAR PLOT 
+      Plotly.newPlot("bar", barTrace, barLayout);
+  
+      // CREAR TRACE PARA BUBBLE PLOT 
+      let bubbleTrace = [{
+        x: dataPlot[1],
+        y: dataPlot [0],
+        text: dataPlot[2], 
+        mode: "markers",
+        marker: {
+      color: [35, 10, 50, 40, 18, 30],
+      colorscale: [[0, 'rgb(200, 255, 200)'], [1, 'rgb(0, 100, 0)']],
+      cmin: 0,
+      cmax: 50,
+      size: [600, 1200, 800, 400, 1500, 2000],
+      sizemode: 'area',
+      showscale: true,
+      colorbar: {
+        thickness: 10,
+        y: 0.5,
+        ypad: 0,
+        title: 'Tree Density',
+        titleside: 'bottom',
+        outlinewidth: 1,
+        outlinecolor: 'black',
+        tickfont: {
+          family: 'Lato',
+          size: 14,
+          color: 'green'
+        }
+      }
+  }
+      }];
+      // CREAR LAYOUT PARA BUBBLE PLOT
+      let bubbleLayout = {
+        title: "Subject´s OTUs population",
+        yaxis: {title: "OTU Value"},
+        xaxis: {title: "OTU ID"},
+        colorway: ['#f3cec9', '#e7a4b6', '#cd7eaf', '#a262a9', '#6f4d96', '#3d3b72', '#182844']
+        
+      };
+      // CREAR BUBBLE PLOT 
+      Plotly.newPlot("bubble", bubbleTrace, bubbleLayout); 
+  
+    };
+      // CREAR TABLA 
+    makePlotsTable();
+  
+  });
